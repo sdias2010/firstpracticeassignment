7649a0109fd47070d4805c9f0c21766e900eaeec
Sharon Dias <sharon.dias2010@gmail.com>
2021-07-11 15:36:08 -0700

    Revert "Modified hello.js"
    This reverts commit bcdf840b009f813499dc03d78134e5d0f8abda31.

--- hello.js
+++ hello.js
@@ -1,12 +1,3 @@
 <script>
 document.getElementById("demo").innerHTML = "Hello Dolly.";
 </script>
-<script>
-let x, y, z;  // Statement 1
-x = 5;        // Statement 2
-y = 6;        // Statement 3
-z = x + y;    // Statement 4
-
-document.getElementById("demo").innerHTML =
-"The value of z is " + z + ".";  
-</script>
